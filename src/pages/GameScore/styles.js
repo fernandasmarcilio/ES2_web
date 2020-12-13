a41a931700d4a2c1d8214711f9c40bc3e8c7e8aa
Fernanda <fernandas.marcilio@edu.univali.br>
2020-12-13 16:22:03 -0300

    create game and score pages

---
+++ src/pages/GameScore/styles.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { Paper, Button } from "@material-ui/core";
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: calc(100vh - 64px);
+  display: flex;
+  padding: 20px 0;
+  justify-content: center;
+  background: var(--color-white);
+`;
+
+export const Card = styled(Paper)`
+  background: var(--color-white);
+  width: 60%;
+  height: 100%;
+  border-radius: 10px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  color: var(--color-primary);
+  font-size: 20px;
+`;
+
+export const GameContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+export const ButtonLogin = styled(Button)`
+  && {
+    margin: 10px 0;
+    height: 56px;
+  }
+`;
+
+export const Text = styled.p`
+  text-align: center;
+  font-size: 18px;
+  margin-top: 10px;
+`;
+
+
--- src/pages/TriviaGame/index.js
+++ src/pages/TriviaGame/index.js
@@ -1,14 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Menu from '../../components/Menu';
 
-//import { Container } from './styles';
+import { RadioGroup, Radio, FormControlLabel, Stepper, Step, StepLabel } from '@material-ui/core';
+import { Container, Card, Title, GameContainer, QuestionText, ButtonLogin, AnswerContainer } from './styles';
+
+const alternatives = [
+  { id: 'a', title: 'Alternativa A' },
+  { id: 'b', title: 'Alternativa B' },
+  { id: 'c', title: 'Alternativa C' },
+  { id: 'd', title: 'Alternativa D' },
+]
+
+const phases = [
+  { id: 0, question: 'O que é um requisito funcional?', alternatives, correctAnswer: 'a' },
+  { id: 1, question: 'O que é um requisito funcional?', alternatives, correctAnswer: 'b' },
+  { id: 2, question: 'O que é um requisito funcional?', alternatives, correctAnswer: 'c' },
+  { id: 3, question: 'O que é um requisito funcional?', alternatives, correctAnswer: 'd' },
+  { id: 4, question: 'O que é um requisito funcional?', alternatives, correctAnswer: 'a' },
+]
+
+const data = { name: 'Trilha 1', phases };
 
 const TriviaGame = () => {
+  const history = useHistory();
+
+  const [gameData, setGameData] = useState(data);
+  const [currentPhase, setCurrentPhase] = useState(0);
+  const [activeStep, setActiveStep] = useState(0);
+  const [userAnswers, setUserAnswers] = useState('');
+  const [allUserAnswers, setAllUserAnswers] = useState([]);
+  const [userScore, setUserScore] = useState(0);
+
+
+  const handleOnChangeInput = (e) => {
+    const { value } = e.target;
+    setUserAnswers(value)
+  };
+
+  const handleChangePhase = (newCurrentPhase) => {
+    setCurrentPhase(newCurrentPhase);
+    setActiveStep(newCurrentPhase);
+    setUserAnswers('');
+    setAllUserAnswers([...allUserAnswers, userAnswers]);
+  }
+
+
+  const handleChangeScore = () => {
+    const correctAnswer = gameData.phases[currentPhase].correctAnswer;
+    if (correctAnswer === userAnswers) setUserScore(userScore + 1);
+  }
+
+  const handleNextQuestion = () => {
+    handleChangeScore();
+    const newCurrentPhase = currentPhase + 1;
+    if (newCurrentPhase < gameData.phases.length) {
+      handleChangePhase(newCurrentPhase);
+    } else {
+      history.push({
+        pathname: '/score',
+        state: { userScore, totalAnswner: gameData.phases.length }
+      });
+    }
+  };
+
   return (
     <>
       <Menu />
-      <h1>Play trivia</h1>
+      <Container>
+        <Card>
+          <Title>{gameData.name}</Title>
+
+          <GameContainer>
+
+            <Stepper alternativeLabel activeStep={activeStep}>
+              {['1', '2', '3', '4', '5'].map(step => (
+                <Step key={step} >
+                  <StepLabel>{''}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+
+            <QuestionText>{gameData.phases[currentPhase].question}</QuestionText>
+
+            <RadioGroup name="correctAnswer" value={userAnswers} onChange={handleOnChangeInput}>
+              {gameData.phases[currentPhase].alternatives.map((alternative) => (
+                <AnswerContainer>
+                  <FormControlLabel value={alternative.id} control={<Radio />} label={alternative.title} />
+                </AnswerContainer>
+              ))}
+            </RadioGroup>
+
+            <ButtonLogin variant="contained" color="primary" onClick={handleNextQuestion}>
+              Responder
+          </ButtonLogin>
+          </GameContainer>
+        </Card>
+      </Container>
     </>
   );
 }
